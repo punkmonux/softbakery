d677735268c7c45bcfb152209942b32a2d532979
Alejandro CM <103467216+punkmonux@users.noreply.github.com>
2023-10-23 00:35:12 -0600

    Update code.py

--- code.py
+++ code.py
@@ -12,6 +12,7 @@ def abrir_segundo_menu():
     global segunda_ventana
     segunda_ventana = tk.Toplevel(root)
     segunda_ventana.title("Menú de Pastelería Yolis")
+    segunda_ventana.configure(bg="#FFDDC1")  # Color de fondo pastel
 
     # Botones en el segundo menú
     botones = ["Betun", "Relleno", "Pan", "Decoracion", "Tamaño", "Forma", "Fondant"]
@@ -29,22 +30,22 @@ def abrir_segundo_menu():
     def mostrar_opciones(tipo):
         seleccion = simpledialog.askstring("Seleccionar opción", f"Selecciona una opción de {tipo}:", parent=segunda_ventana)
         if seleccion:
-            tk.Label(segunda_ventana, text=f"Seleccionaste: {seleccion}").pack(padx=20, pady=10)
+            tk.Label(segunda_ventana, text=f"Seleccionaste: {seleccion}", bg="#FFDDC1").pack(padx=20, pady=10)
 
     for boton in botones:
-        tk.Button(segunda_ventana, text=boton, command=lambda b=boton: mostrar_opciones(b)).pack(padx=20, pady=10)
+        tk.Button(segunda_ventana, text=boton, bg="#FFABAB", command=lambda b=boton: mostrar_opciones(b)).pack(padx=20, pady=10)
 
     # Botón para enviar y cerrar el segundo menú
     def enviar_y_cerrar():
-        tk.Label(segunda_ventana, text="Enviado exitosamente, ir a cocina!!!").pack(padx=20, pady=10)
+        tk.Label(segunda_ventana, text="Enviado exitosamente, ir a cocina!!!", bg="#FFDDC1").pack(padx=20, pady=10)
         segunda_ventana.destroy()
         root.deiconify()
 
-    boton_enviar = tk.Button(segunda_ventana, text="Enviar", command=enviar_y_cerrar)
+    boton_enviar = tk.Button(segunda_ventana, text="Enviar", bg="#FFC3A0", command=enviar_y_cerrar)
     boton_enviar.pack(padx=20, pady=10)
 
     # Botón para cerrar el segundo menú sin enviar
-    boton_cerrar = tk.Button(segunda_ventana, text="Cerrar", command=cerrar_segundo_menu)
+    boton_cerrar = tk.Button(segunda_ventana, text="Cerrar", bg="#FF677D", command=cerrar_segundo_menu)
     boton_cerrar.pack(padx=20, pady=20)
 
 # Función para cerrar la aplicación
@@ -54,19 +55,19 @@ def cerrar_aplicacion():
 # Configuración de la ventana principal
 root = tk.Tk()
 root.title("Pastelería Yolis")
+root.configure(bg="#FFDDC1")  # Color de fondo pastel
 
 # Etiqueta en el menú principal
-label_menu_principal = tk.Label(root, text="Pastelería Yolis", font=("Helvetica", 24))
+label_menu_principal = tk.Label(root, text="Pastelería Yolis", font=("Helvetica", 24), bg="#FFDDC1")  # Color de fondo pastel
 label_menu_principal.pack(padx=20, pady=20)
 
 # Botón para iniciar y abrir el segundo menú
-boton_iniciar = tk.Button(root, text="Iniciar", command=abrir_segundo_menu)
+boton_iniciar = tk.Button(root, text="Iniciar", bg="#FFABAB", command=abrir_segundo_menu)  # Color botón pastel
 boton_iniciar.pack(padx=20, pady=20)
 
 # Botón para cerrar la aplicación
-boton_cerrar_app = tk.Button(root, text="Cerrar Aplicación", command=cerrar_aplicacion)
+boton_cerrar_app = tk.Button(root, text="Cerrar Aplicación", bg="#FF677D", command=cerrar_aplicacion)  # Color botón pastel
 boton_cerrar_app.pack(padx=20, pady=20)
 
 # Iniciar el bucle principal de la aplicación
 root.mainloop()
-
